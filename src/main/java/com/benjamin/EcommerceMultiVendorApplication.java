975a0cfa1b0e2daebed9018966ecbd1c0c4c195f
Benjamin <Benjamin@gmail.com>
2025-07-05 10:52:59 +0700

    first commit

---
+++ src/main/java/com/benjamin/EcommerceMultiVendorApplication.java
@@ -0,0 +1,13 @@
+package com.benjamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMultiVendorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMultiVendorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benjamin/EcommerceMultiVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMultiVendorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
